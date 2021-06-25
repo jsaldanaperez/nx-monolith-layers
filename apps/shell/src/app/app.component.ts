3020d32466dcd3be35ac597ea6641ed96d9c3b44
jperez <j.saldanaperez@gmail.com>
2021-06-25 13:57:22 +0200

    initial commit

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-monolith-layers-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shell';
+}
